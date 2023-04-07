b19dcd3c334b3148d29af52262e1a5421df8ce62
정재훈 <wogns8123@naver.com>
2023-04-07 17:48:45 +0900

    q

--- 아파트 - 도서관.py
+++ 아파트 - 도서관.py
@@ -1,12 +1,13 @@
 import mysql.connector
 from geopy.distance import geodesic
+import pandas as pd
 
 # MySQL 연결 정보
 config = {
     'user': 'iujuser',
     'password': '!uj2023',
     'host': 'db-iuj.cj557j0pfgbg.ap-northeast-2.rds.amazonaws.com',
-    'database': 'iujdev',
+    'database': 'IUJ',
     'raise_on_warnings': True
 }
 
@@ -17,54 +18,57 @@ cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor(prepared=True)
 
 # 빌라 쿼리 실행
-villa_query = "SELECT id, sigungu, lat, lng FROM apt WHERE id >= 30000"
-cursor.execute(villa_query)
-villa = cursor.fetchall()
+_query = "SELECT id, title, description, school, local, pub_date, link, similar_list FROM news"
+cursor.execute(_query)
+news = cursor.fetchall()
 
-# 도서관 쿼리 실행
-library_query = "SELECT id, name, lat, lng FROM library"
-cursor.execute(library_query)
-library = cursor.fetchall()
+df = pd.read_csv("")
 
-# 연결 종료
-# 결과 출력
-for a in villa:
-    home_location = (a[2], a[3])
-    home_id = a[0]
-    nearest_library_id = None
-    min_distance = float('inf')
+
+# # 도서관 쿼리 실행
+# library_query = "SELECT id, name, lat, lng FROM library"
+# cursor.execute(library_query)
+# library = cursor.fetchall()
+
+# # 연결 종료
+# # 결과 출력
+# for a in villa:
+#     home_location = (a[2], a[3])
+#     home_id = a[0]
+#     nearest_library_id = None
+#     min_distance = float('inf')
     
-    # Find the nearest cinema
-    for c in library:
-        library_location = (c[2], c[3])
-        library_id = c[0]
-        distance = geodesic(home_location, library_location).km
-        if distance < min_distance:
-            min_distance = distance
-            nearest_library_id = library_id
+#     # Find the nearest cinema
+#     for c in library:
+#         library_location = (c[2], c[3])
+#         library_id = c[0]
+#         distance = geodesic(home_location, library_location).km
+#         if distance < min_distance:
+#             min_distance = distance
+#             nearest_library_id = library_id
     
-    # Calculate the score based on the distance to the nearest cinema
-    if '서울' in a[1]:
-        cutoff_distance = 0.5
-        max_score = 100
-        score = max(0, round(max_score - (min_distance - cutoff_distance) / 0.01))
-    elif "부산" in a[1] or '대구' in a[1] or '인천' in a[1] or '광주' in a[1] or '대전' in a[1] or '울산' in a[1] or '경기도' in a[1]:
-        cutoff_distance = 0.75
-        max_score = 100
-        score = max(0, round(max_score - (min_distance - cutoff_distance) / 0.015))
-    else:
-        cutoff_distance = 1
-        max_score = 100
-        score = max(0, round(max_score - (min_distance - cutoff_distance) / 0.02))
-    if score > 100:
-        score = 100
-    print(a[1],min_distance,home_id,score)
+#     # Calculate the score based on the distance to the nearest cinema
+#     if '서울' in a[1]:
+#         cutoff_distance = 0.5
+#         max_score = 100
+#         score = max(0, round(max_score - (min_distance - cutoff_distance) / 0.01))
+#     elif "부산" in a[1] or '대구' in a[1] or '인천' in a[1] or '광주' in a[1] or '대전' in a[1] or '울산' in a[1] or '경기도' in a[1]:
+#         cutoff_distance = 0.75
+#         max_score = 100
+#         score = max(0, round(max_score - (min_distance - cutoff_distance) / 0.015))
+#     else:
+#         cutoff_distance = 1
+#         max_score = 100
+#         score = max(0, round(max_score - (min_distance - cutoff_distance) / 0.02))
+#     if score > 100:
+#         score = 100
+#     print(a[1],min_distance,home_id,score)
         
 
-    score_query = f"UPDATE score SET library = %s WHERE id = {home_id+30000} AND type='apt'"
+#     score_query = f"UPDATE score SET library = %s WHERE id = {home_id+30000} AND type='apt'"
     
-    cursor.execute(score_query, [score])
-    cnx.commit()
+#     cursor.execute(score_query, [score])
+#     cnx.commit()
 
 
 
