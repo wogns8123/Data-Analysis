147e5c4c54882ea4b20c5dc7d513ed31b784f716
정재훈 <wogns8123@naver.com>
2023-04-10 18:02:22 +0900

    캐글 데이터분석

---
+++ 캐글 - 데이터분석 시작하기/1.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import keras
+import sklearn
+plt.style.use('seaborn')
+sns.set(font_scale=2.5)
+import missingno as msno
+import warnings
+# warnings.filterwarnings('ignore')
+
+%matplotlib inline
